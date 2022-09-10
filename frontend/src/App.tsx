e6e275bb730dd7fa01e671c287ab537b86cfeb5f
isabelly <isabellyd429@gmail.com>
2022-09-10 16:21:42 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-
+import NotificationButton from "./components/notification-button"
 
 function App() {
-  
-  return(
-    <h1>Ola mundo!</h1>
+
+  return (
+    <>
+      <h1>Ola mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
