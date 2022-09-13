b95c3c9c53fbea5aad562cfa4992851b9dedea1e
isabelly <isabellyd429@gmail.com>
2022-09-12 23:07:42 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/controller/ControllerSales.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/ControllerSales.java
@@ -1,9 +1,15 @@
 package com.devsuperior.dsmeta.controller;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.dsmeta.model.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
@@ -19,9 +25,15 @@ public class ControllerSales {
 	
 	
 	@GetMapping
-	public List<Sale> findSale(){
+	public Page<Sale> findSale( @RequestParam(value = "minDate", defaultValue = "")String minDate, @RequestParam(value = "maxDate", defaultValue = "" )
+	String maxDate, Pageable pageable){
+
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(maxDate); LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 		
-		return repository.findAll();
+		
+		return repository.findSales(min, max, pageable);
 	}
 	
 
--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -2,12 +2,18 @@ package com.devsuperior.dsmeta.repositories;
 
 
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.model.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
-	
-	
+	//selecionando as vendas que as datas estejam entre o param min e max
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
